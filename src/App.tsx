da0bd36db2627234ec776c61237034114291caac
Dominik Kolarsky <kolarskydominik@gmail.com>
2023-10-16 11:18:50 +0200

    test css, js app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import './App.css';
+
 function App({ domElement }: { domElement: HTMLElement }) {
   const attribute = domElement.getAttribute('data-attribute-1');
 
   return (
     <>
-      <div>Hello from micro-frontend-app, called: {attribute}</div>
+      <div className="tkd_calendar">
+        <div className="tkd_calendar__inner">
+          <h2>Hello from micro-frontend-app, called: {attribute}</h2>
+        </div>
+      </div>
     </>
   );
 }
