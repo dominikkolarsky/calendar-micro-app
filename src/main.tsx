b26cf3b4d789a05cfedd3732cd464441f641f839
Dominik Kolarsky <kolarskydominik@gmail.com>
2023-10-16 10:13:48 +0200

    cleanup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const widgetName = 'name-of-micro-app';
+const widgetDiv = document.getElementById(widgetName);
+
+ReactDOM.createRoot(widgetDiv!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <App domElement={widgetDiv!} />
+  </React.StrictMode>
+);
